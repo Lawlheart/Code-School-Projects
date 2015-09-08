804ef0dc9a5a09df61942900d9064a55a024cf82
LawlietBlack <kenneth.black@live.com>
2015-09-07 22:29:12 -0700

    Completed Stage 3 of 5 of Shaping up with AngularJS

--- angularjs/js/app.js
+++ angularjs/js/app.js
@@ -9,14 +9,25 @@ app.controller('StoreController', function() {
 				description: '. . .',
 				canPurchase: false,
 				soldOut: true,
-				image: 'img/gem-03.gif' 
+				image: 'img/gem-03.gif', 
 			}, {
 				name: 'Pentagonal Gem',
 				price: 5.95,
 				description: 'Origin of the Pentagonal Gem is unknown, hence it\'s low value. It has a very high shine and 12 sides',
 				canPurchase: false,
 				soldOut: false,
-				image: 'img/gem-01.gif'
+				image: 'img/gem-01.gif',
+				reviews: [
+					{
+						author: 'John@example.com',
+						body: 'I love it!',
+						stars: 5
+					}, {
+						author: 'Louise@example.com',
+						body: 'It\'s adequate.',
+						stars: 3
+					}
+				]
 			}
 		];
 	});
@@ -31,7 +42,13 @@ app.controller('PanelController', function() {
 		}
 });
 
-
+app.controller('ReviewController', function() {
+	this.review = {};
+	this.addReview = function(product) {
+		product.reviews.push(this.review);
+		this.review = {};
+	}
+});
 
 
 
