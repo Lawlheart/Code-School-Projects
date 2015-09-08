51b0774aeacb81f46fe2b47ab9c7c443c4fadd50
LawlietBlack <kenneth.black@live.com>
2015-09-08 00:00:59 -0700

    Completed Shaping up with AngularJS

--- angularjs/js/app.js
+++ angularjs/js/app.js
@@ -1,60 +1,25 @@
 (function() {
-var app = angular.module('store', []);
+var app = angular.module('store', ['store-products']);
 
-app.controller('StoreController', function() {
-		this.products = [
-			{
-				name: 'Dodecahedron',
-				price: 2.95,
-				description: '. . .',
-				canPurchase: false,
-				soldOut: true,
-				image: 'img/gem-03.gif', 
-			}, {
-				name: 'Pentagonal Gem',
-				price: 5.95,
-				description: 'Origin of the Pentagonal Gem is unknown, hence it\'s low value. It has a very high shine and 12 sides',
-				canPurchase: false,
-				soldOut: false,
-				image: 'img/gem-01.gif',
-				reviews: [
-					{
-						author: 'John@example.com',
-						body: 'I love it!',
-						stars: 5
-					}, {
-						author: 'Louise@example.com',
-						body: 'It\'s adequate.',
-						stars: 3
-					}
-				]
-			}
-		];
+app.controller('StoreController', ['$http', function($http) {
+	var store = this;
+	store.products = [];
+	$http.get('js/products.json').success(function(data) {
+		store.products = data;
 	});
-
-app.controller('PanelController', function() {
-		this.tab = 1;
-		this.selectTab = function(setTab) {
-			this.tab = setTab;
-		};
-		this.isSelected = function(checkTab) {
-			return this.tab === checkTab;
-		}
-});
+}]);
 
 app.controller('ReviewController', function() {
 	this.review = {};
 	this.addReview = function(product) {
 		product.reviews.push(this.review);
 		this.review = {};
-	}
+	};
 });
 
+app.factory('', ['$http', function() {
 
 
-
-
-
-
+}]);
 
 })();
